34d6aae24e334df412d48ec5c7e722db5fa6e4ff
Bishwa <bishwa.nayak@sap.com>
2020-02-11 20:19:34 +0530

    redux changes

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,13 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+// Redux
+import {Provider }  from 'react-redux';
+import store from './store';
 
-const App = ()  => 
+
+const App = ()  => (
+  <Provider store ={store}>
     <Router>
       <Fragment>
         <Navbar />
@@ -20,6 +25,7 @@ const App = ()  =>
         </section>
       </Fragment>
     </Router>
-
+    </Provider>
+);
 
 export default App;
